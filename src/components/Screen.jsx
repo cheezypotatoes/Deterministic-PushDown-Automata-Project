ec3d74fb48cb539ce8b7f2115f4d2f7dd9f4da9c
jorge <toxicpotato1223@gmail.com>
2024-12-06 17:06:02 +0800

    Added deploy yml

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from 'react';
 export default function Screen() {
     const [statesOnScreen, SetStatesOnScreen] = useState([]);
     const [positions, setPositions] = useState({});
+    console.log("Ee")
     
     useEffect(() => {
         const addState = (event) => {
